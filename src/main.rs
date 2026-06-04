2623cf99d7155bbb5b9b8c4dce49a109733d8467
vitaly <vitaly.neumoin@gmail.com>
2026-06-05 00:59:21 +0300

    Удалён модуль `domain`, обновлена ссылка на `App` через модуль `app` в `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+pub mod app;
 
 use std::io;
 
@@ -7,22 +7,11 @@ use color_eyre::{
     Result,
 };
 
-use arithmet::domain::banner;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    DefaultTerminal, Frame,
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
-};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let mut terminal = ratatui::init();
-    let app_result = App::default().run(&mut terminal);
+    let app_result = app::App::default().run(&mut terminal);
     ratatui::restore();
     app_result
 }
